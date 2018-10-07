eba7b6e6fdcee7c606a4b52e43e109bd0f039d09
Ryan Proud <ryan.proud@binarymelon.com>
2018-10-07 12:28:02 -0300

    add simple spring boot server for browser files

---
+++ servepkx-spring-boot/src/main/java/io/github/flagbrew/servepkx/Application.java
@@ -0,0 +1,16 @@
+package io.github.flagbrew.servepkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rproud
+ * @since 2018-10-07
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
